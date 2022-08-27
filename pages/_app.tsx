04ce033de2f75e475ba1011640652d8bf5e60984
xiaomitenshi <93534067+xiaomitenshi@users.noreply.github.com>
2022-08-27 15:07:38 +0530

    made changes
    front design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { alchemyProvider } from "wagmi/providers/alchemy"
 import { publicProvider } from "wagmi/providers/public"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import styles from '../styles/Home.module.css';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.polygonMumbai],
@@ -38,6 +39,29 @@ function MyApp({ Component, pageProps }: AppProps) {
       <RainbowKitProvider chains={chains}>
         <Header />
         <Component {...pageProps} />
+        <div class={styles.front}>
+          <div class={styles.frontpage}>
+            <div class={styles.introtext}>
+              <ul>Redeem your</ul>
+              <ul>DAO Tokens</ul>
+              <div class={styles.subheading}>
+                Perform Tasks, Earn Rewards and Redeem with Merch or Tickets!
+              </div>
+              <div class={styles.button}>Visit Marketplace</div>
+            </div>
+            <div class={styles.imghe}>
+              <img src="https://firebasestorage.googleapis.com/v0/b/logpro-f2137.appspot.com/o/1.png?alt=media&token=2b17acaa-947c-4851-a55c-306d0d5ee56c" />
+            </div>
+          </div>
+          <div class={styles.rear}>
+            <div class={styles.re}>
+              <ul>Making your community active</ul>
+              <ul>is now easier with</ul>
+              <ul>XXXXXXXXX</ul>
+            </div>
+            <div class={styles.button2}>Create your token</div>
+          </div>
+        </div>
         <Footer />
       </RainbowKitProvider>
     </WagmiConfig>
